f850a0ac82669df29bf15a987b3c1cd39ae5fea5
Pascal Pieper <accounts@pascalpieper.de>
2021-10-29 15:07:54 +0200

    Input/Output works

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <USBComposite.h>
 
 static constexpr int8_t base_note = 60;
+
+constexpr bool invert_input = false;
 constexpr uint8_t INPUT_PINS[] = {
     PA0,
     PA1,
@@ -11,6 +13,7 @@ constexpr uint8_t INPUT_PINS[] = {
     PA5,
 };
 
+// At 3.3v, my button LEDs need around 25 Ohm to be driven with 5mA
 constexpr bool invert_output = true;
 constexpr uint8_t OUTPUT_PINS[] = {
     PB8,
@@ -29,11 +32,16 @@ class myMidi : public USBMIDI {
      if(note >= base_note && (note - base_note) < sizeof(OUTPUT_PINS)) {
          digitalWrite(OUTPUT_PINS[note - base_note], 0 ^ invert_output);
      }
+     digitalWrite(PC13, 1); //debug
  }
  virtual void handleNoteOn(unsigned int channel, unsigned int note, unsigned int velocity) {
      if(note >= base_note && (note - base_note) < sizeof(OUTPUT_PINS)) {
          digitalWrite(OUTPUT_PINS[note - base_note], 1 ^ invert_output);
      }
+     digitalWrite(PC13, 0); //debug
+  }
+  virtual void handleControlChange(unsigned int channel, unsigned int controller, unsigned int value) {
+    // TODO: Funny Patterns
   }
 
 };
@@ -44,11 +52,13 @@ USBCompositeSerial CompositeSerial;
 void setup() {
     for(const auto pin : OUTPUT_PINS) {
         pinMode(pin, OUTPUT);
-        digitalWrite(pin, 1);
+        digitalWrite(pin, 0 ^ invert_output);
     }
     for(const auto pin : INPUT_PINS) {
-        pinMode(pin, INPUT);
-        digitalWrite(pin, 0);   //pull-down
+        if(invert_input)
+            pinMode(pin, INPUT_PULLUP);
+        else
+            pinMode(pin, INPUT_PULLDOWN);
     }
     USBComposite.setProductId(0x0031);
     midi.registerComponent();
@@ -59,12 +69,15 @@ void setup() {
 void loop() {
     midi.poll();
     for(uint8_t i = 0; i < sizeof(INPUT_PINS); i++) {
-        uint8_t meas = digitalRead(INPUT_PINS[i]);
+        uint8_t meas = digitalRead(INPUT_PINS[i]) ^ invert_input;
         if(input_state[i] != meas) {
-            if(meas)
+            if(meas) {
                 midi.sendNoteOn(0, base_note + i, 127);
-            else
+                digitalWrite(PC13, 0); //debug
+            } else {
                 midi.sendNoteOff(0, base_note + i, 127);
+                digitalWrite(PC13, 1); //debug
+            }
             input_state[i] = meas;
         }
     }
